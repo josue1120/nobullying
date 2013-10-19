f291c07af738e953e4e97fefd7dc66209250b3eb
Josue Daniel Bustamante <josue1120@outlook.com>
2013-10-18 23:57:31 -0400

    Pasando todo a Expressjs

---
+++ NoBullying/public/javascripts/js.js
@@ -0,0 +1,22 @@
+describe('OAuth2',function(){
+	var OAuth = require('OAuth');
+
+	it('gets bearer token', function(done){
+		var OAuth2 = OAuth.OAuth2;    
+		var twitterConsumerKey = 'your key';
+		var twitterConsumerSecret = 'your secret';
+		var oauth2 = new OAuth2(server.config.keys.twitter.consumerKey,
+			twitterConsumerSecret, 
+			'https://api.twitter.com/', 
+			null,
+			'oauth2/token', 
+			null);
+		oauth2.getOAuthAccessToken(
+			'',
+			{'grant_type':'client_credentials'},
+			function (e, access_token, refresh_token, results){
+			console.log('bearer: ',access_token);
+			done();
+		});
+	});
+});
--- README.md
+++ README.md
@@ -2,3 +2,5 @@ NoBullying
 ==========
 By: 
 @JosueDanielBust & @jdavidavendano
+
+To run the node.js server you must write in console *bash server.sh*
